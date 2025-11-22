2a233cd253a420a088f9c00d5b63afa026f4bdd4
duane-llose <duane.llose@yahoo.com>
2025-11-22 00:23:15 -0800

    Changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import Button from './UI/Button.tsx';
+import { useTimersContext } from '../store/timers-context.tsx';
 
 export default function Header() {
+  const timersCtx = useTimersContext();
+  const isRunning = timersCtx.isRunning;
   return (
     <header>
       <h1>ReactTimer</h1>
-
-      <Button>Stop Timers</Button>
+      <Button onClick={isRunning ? timersCtx.stopTimers : timersCtx.startTimers}>{isRunning ? 'Stop' : 'Start'} Timers</Button>
     </header>
   );
 }
